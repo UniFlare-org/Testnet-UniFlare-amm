ca296ebebe76354f9898262c9c1672045b072ee8
goose <admin@gooseswap.finance>
2021-06-08 19:01:04 -0400

    first commit

---
+++ src/constants/displayPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0xa7BAFB9441d88708D082e4Be1878bC2611ACf774', // asset being valued
+  busdAddress: '0x48925b156efed39da465c6cb5e2f9d9a93a02309', // asset assumed as value
+  lpAddress: '0xA526083c6F9e87b87Cc8265E2c6E5174363a6E92' // LP
+}
+
+export default priceContracts
